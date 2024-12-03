765b11607548c432d7ed45e90e1f4c395346e8fa
MustaGitRefresh <kabcoco.2gitdom.3@gmail.com>
2024-12-03 16:33:58 +0530

    New java files

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
-import org.w3c.dom.html.HTMLImageElement;
 
 import java.util.Scanner;
 
-
-class Node{
+class Node {
     int data;
     Node link;
 
-    Node(){
+    Node() {
         data = 0;
         link = null;
     }
 
-    void create(){
+    void create() {
         // creating linked list
         int n;
         Scanner in = new Scanner(System.in);
@@ -22,7 +20,7 @@ class Node{
         n = in.nextInt();
         Node temp;
         Node ptr = this;
-        for(int i=0;i<n;i++) {
+        for (int i = 0; i < n; i++) {
             temp = new Node();
             System.out.println("Enter next data");
             temp.data = in.nextInt();
@@ -31,21 +29,23 @@ class Node{
             ptr = ptr.link;
         }
     }
-    Node insertBeg(Node start, int x){
+
+    Node insertBeg(Node start, int x) {
         // inserting the list in the beginning
         Node temp = new Node();
         // Entering the data into the new list
         temp.data = x;
-        temp.link  = start;
+        temp.link = start;
         return temp;
     }
-    Node insertMid(Node start, int x, int n){
+
+    Node insertMid(Node start, int x, int n) {
         // create temp
         Node temp = new Node();
         temp.data = x;
         Node ptr = start;
         int c = 0;
-        while(c<n){
+        while (c < n) {
             ptr = ptr.link;
             c++;
         }
@@ -54,20 +54,22 @@ class Node{
         start = ptr;
         return start;
     }
-    void insertEnd(Node start, int x){
+
+    void insertEnd(Node start, int x) {
         Node temp = new Node();
         temp.data = x;
         Node ptr = start;
-        while (ptr.link != null){
+        while (ptr.link != null) {
             ptr = ptr.link;
         }
         ptr.link = temp;
     }
-    void delete(Node start, int n){
+
+    void delete(Node start, int n) {
         Node ptr = start;
         Node ptr1 = start;
         int c = 0;
-        while(c<n){
+        while (c < n) {
             ptr1 = ptr;
             ptr = ptr.link;
             c++;
@@ -75,20 +77,22 @@ class Node{
         ptr1.link = ptr.link;
         ptr.link = null;
     }
-    void display(){
+
+    void display() {
         Node ptr = this;
         int counter = 0;
-        while(ptr!=null){
+        while (ptr != null) {
             System.out.println(ptr.data);
-            if(checkEven(ptr.data)){
+            if (checkEven(ptr.data)) {
                 counter++;
             }
             ptr = ptr.link;
         }
         System.out.println("The number of even nodes are " + counter);
     }
-    boolean checkEven(int number){
-        return number%2==0;
+
+    boolean checkEven(int number) {
+        return number % 2 == 0;
     }
 }
 
@@ -99,9 +103,8 @@ public class Main {
         node = node.insertBeg(node, 253);
         node = node.insertMid(node, 233, 1);
         node.insertEnd(node, 2000);
-//        node.delete(node, 2);
+        // node.delete(node, 2);
         System.out.println("Displaying the data");
         node.display();
     }
 }
-
